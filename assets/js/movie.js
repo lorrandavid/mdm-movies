5046e8e50df63fcaf1e18afb08ac3ba2ecd8b336
Lorran David <lorrantd1@gmail.com>
2018-02-08 11:49:10 -0200

    Created Movie Class

---
+++ assets/js/movie.js
@@ -0,0 +1,35 @@
+export default class Movie {
+  constructor(key) {
+    this.key = key;
+  }
+
+  /**
+   * get movies list
+   */
+  movies() {
+    // ajax
+  }
+
+  /**
+   * get details single movie
+   * @params: id(Int)
+   */
+  find() {
+    // ajax
+  }
+
+  /**
+   * get discover list
+   */
+  discover() {
+    // ajax
+  }
+
+  /**
+   * get favorite movies
+   * @params: arr(Array)
+   */
+  favorites(arr) {
+    // get favorite movies
+  }
+}
